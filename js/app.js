763abbba61dfdcd3daec583d57a80844836439f9
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-18 17:46:13 -0500

    create cached element references with type asseetions

--- js/app.js
+++ js/app.js
@@ -6,5 +6,15 @@ let turn;
 let winner;
 let tie;
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr");
+console.log(squareEls);
+const messageEl = document.querySelector("#message");
+console.log(messageEl);
+const resetBtnEl = document.querySelector("#reset-button");
+console.log(resetBtnEl);
 /*----------------------------- Event Listeners -----------------------------*/
+// resetBtnEl?.addEventListener('click', handleResetClick)
 /*-------------------------------- Functions --------------------------------*/
+// function handleResetClick(evt: MouseEvent): void {
+//   console.log(evt)
+// }
--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,24 @@ let turn: number
 let winner: boolean
 let tie: boolean
 
-
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelectorAll(".sqr") as NodeListOf<HTMLDivElement>;
+console.log(squareEls)
 
+const messageEl = document.querySelector("#message") as HTMLDivElement;
+console.log(messageEl) 
+
+const resetBtnEl = document.querySelector("#reset-button") as HTMLButtonElement;
+console.log(resetBtnEl);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+// resetBtnEl?.addEventListener('click', handleResetClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+// function handleResetClick(evt: MouseEvent): void {
+//   console.log(evt)
+// }
